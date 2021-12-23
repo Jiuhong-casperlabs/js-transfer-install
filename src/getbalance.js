ce6760aef5ace554177cf7406260b1aa226b290e
Jiuhong <jiuhong@casperlabs.io>
2021-12-23 18:13:08 +0800

    update pk

--- src/getbalance.js
+++ src/getbalance.js
@@ -5,19 +5,32 @@ import { CasperClient,CLPublicKey,CLPublicKeyTag } from "casper-js-sdk";
 const main = async () => {
   const client = new CasperClient(constants.DEPLOY_NODE_ADDRESS);
 
-  const rawEd25519Account = Uint8Array.from([
-    154, 211, 137, 116, 146, 249, 164, 57,
-    9, 35, 64, 255, 83, 105, 131, 86,
-    169, 250, 100, 248, 12, 68, 201, 17,
-    43, 62, 151, 55, 158, 87, 186, 148
-  ]);
+  // method 1
+  // const rawEd25519Account = Uint8Array.from([
+  //   154, 211, 137, 116, 146, 249, 164, 57,
+  //   9, 35, 64, 255, 83, 105, 131, 86,
+  //   169, 250, 100, 248, 12, 68, 201, 17,
+  //   43, 62, 151, 55, 158, 87, 186, 148
+  // ]);
     
-  const publicKeyEd25519 = new CLPublicKey(
-    rawEd25519Account,
-    CLPublicKeyTag.ED25519
-  );
+  // const publicKeyEd25519 = new CLPublicKey(
+  //   rawEd25519Account,
+  //   CLPublicKeyTag.ED25519
+  // );
   
-  const aa = await client.balanceOfByPublicKey(publicKeyEd25519);
+  // const aa = await client.balanceOfByPublicKey(publicKeyEd25519);
+  // console.log(aa.toNumber());
+
+  // method 2
+  const publicKey = CLPublicKey.fromHex(
+    '0152836c51eac04205bb7febe9d92da50758178b0bf388bd03e1da13147b99e2c5'
+  );
+
+  const aa = await client.balanceOfByPublicKey(publicKey);
   console.log(aa.toNumber());
+  // const accountHex = publicKey.toHex();
+  // console.log("publicKey is: ", publicKey)
+  // console.log("accountHex is: ",accountHex)
+
 }
 main();
