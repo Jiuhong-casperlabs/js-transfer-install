3c338c6a229640abe9e036a427df2b48c11c8f27
Jiuhong <jiuhong@casperlabs.io>
2022-07-07 11:14:42 +0800

    update getStateKeyValue

--- src/getStateKeyValue.js
+++ src/getStateKeyValue.js
@@ -14,11 +14,20 @@ const main = async () => {
   const statekey = "hash-3ed9e9188eec90d313a270b81a5ee414f6ecaa9ab9dc8581362f52b73a8655a0"
   const statePath = "myaccount"
 
-  
+  // const PATH_TO_CONTRACTS = "/home/jh/rust/test72/contract/target/wasm32-unknown-unknown/release/contract.wasm";
   const result = await client.nodeClient.getBlockState(stateRootHash, statekey, [
     statePath,
   ]);
   console.log("result is ", result.Account)
+
+  const statekey1 = "account-hash-9f2d00edfce09a6743061b4b557049eb735040cc57ad2d924ad85220cbef77aa" //account hash
+  const statePath1 = "mycontracthash"
+
+  // const PATH_TO_CONTRACTS = "/home/jh/rust/test72/contract/target/wasm32-unknown-unknown/release/contract.wasm";
+  const result1 = await client.nodeClient.getBlockState(stateRootHash, statekey1, [
+    statePath1,
+  ]);
+  console.log("result is ", result1.Contract)
 
 };
 
