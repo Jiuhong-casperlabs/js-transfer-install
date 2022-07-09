20c95e706c87a9fae655df1be5dc39ebcc01d350
Jiuhong <jiuhong@casperlabs.io>
2022-07-09 08:42:18 +0800

    add dictU256

--- src/querycontractdic.js
+++ src/querycontractdic.js
@@ -1,20 +1,18 @@
 import {
   CasperClient,
   Contracts,
-  CLAccountHash,
-  CLKey,
-  CLValue
+  CLU256
 } from "casper-js-sdk";
 
 const main = async () => {
 
-  // const client = new CasperClient("http://localhost:11101/rpc");
-  const client = new CasperClient("http://16.162.124.124:7777/rpc");
+  const client = new CasperClient("http://localhost:11101/rpc");
+  // const client = new CasperClient("http://16.162.124.124:7777/rpc");
 
   const { Contract } = Contracts;
   
   const contractClient = new Contract(client);
-  const contract_hash = "hash-fe7b2d00804de7800da0115945df5fb662898455fe163934edc238c0473680da" // contract hash - testnet
+  const contract_hash = "hash-3a2c0d6683291c5a39e4163f75a1684352551590b8cfb18c488b0d127b76d059" // contract hash - testnet
   // const contract_hash = "hash-2d29c667c66d35fc4a40c73eb5d86af2ca6741d99bfb67ae2854321faffde3c7" //contract hash - nctl
   contractClient.setContractHash(contract_hash)
   
@@ -22,8 +20,11 @@ const main = async () => {
   console.log("queryContractDictionary")
   const result = await contractClient.queryContractDictionary("my_dicname", "mydicitemkey")
 
-  console.log(`account-hash-${ Buffer.from(result.data).toString("hex")}`)
+  console.log(`account-hash-${Buffer.from(result.data).toString("hex")}`)
   
+  const result2 = await contractClient.queryContractDictionary("my_dicname", "U256Key")
+
+  console.log("result2 is ", result2.data.toString())
 
 };
 
