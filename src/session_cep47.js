52b83f28f3710fe91acc4209115087aee95c7ec3
Jiu-hong <jiuhong@casperlabs.io>
2023-01-05 19:45:18 +0800

    u

--- src/session_cep47.js
+++ src/session_cep47.js
@@ -38,10 +38,20 @@ const main = async () => {
 
   const token_ids = new CLList([new CLU256(1), new CLU256(2), new CLU256(3)]);
 
+  const meta_data_json = {
+    name: "John Doe",
+    symbol: "abc",
+    token_uri: "https://www.barfoo.com",
+  };
+
+  const multiple_meta_data = new CLString(JSON.stringify(meta_data_json));
+
+
   const token_meta1 = new CLMap([[new CLString("a"), new CLString("aa")]]);
   const token_meta2 = new CLMap([[new CLString("b"), new CLString("bb")]]);
-  const token_meta3 = new CLMap([[new CLString("c"), new CLString("cc")]]);
+  const token_meta3 = new CLMap([[new CLString("c"), multiple_meta_data]]);
   const token_metas = new CLList([token_meta1, token_meta2, token_meta3]);
+
 
   const hexString =
   "010e31a03ea026a8e375653573e0120c8cb96699e6c9721ae1ea98f896e6576ac3";
@@ -49,7 +59,6 @@ const main = async () => {
 
   const accounthash = new CLAccountHash(hash);
   const receipient = new CLKey(accounthash);
-
   let deploy = DeployUtil.makeDeploy(
     new DeployUtil.DeployParams(
       keyPairofContract.publicKey,
@@ -78,7 +87,7 @@ const main = async () => {
 
   console.log(`mint_one -- deploy hash = ${deployHash}`);
 };
-
+2500000000
 main();
 
 //https://testnet.cspr.live/deploy/9d242efa3fab8b5191a2eabe8922da9d4fa9f9f94328efecb31fe6de09f57d15
