5c2bae4cb450050c41290cedb6defdcbed3c819c
Jiuhong <jiuhong@casperlabs.io>
2023-12-15 11:19:54 +0800

    update commmet

--- src/querycontractdic.js
+++ src/querycontractdic.js
@@ -1,9 +1,11 @@
-import { CasperClient, Contracts, CLU256 } from "casper-js-sdk";
+import { CasperClient, Contracts } from "casper-js-sdk";
 
 const ACCOUNT_HASH =
   "8d288f9a28f942afe39a45fce375bc3c1026c8f97acdd4de14e6bfcc3f707322";
 const CONTRACT_HASH =
   "hash-5980eae4c01d536a0db6de3ec3bec87d9d1b7887805e224e940e45227cc4d83c";
+
+// query cep47 balance
 const main = async () => {
   const client = new CasperClient("http://localhost:11101/rpc");
 
